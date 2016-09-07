057303cfbc59381a2cd045587a7032828e7bf99c
Benjamin Donald Antonsen <antonsenbd@dcsdk12.org>
2016-09-07 12:09:51 -0600

    Implement has_teen

--- quiz.py
+++ quiz.py
@@ -1,5 +1,13 @@
 	
 # TODO - write has_teen
+def has_teen(a, b, c):
+	if a >= 13 and a <= 19 or b >= 13 and b <= 19 or c >= 13 and c <= 19:
+		return True
+	else:
+		return False
+print has_teen(13, 14, 15) #True
+print has_teen(12, 13, 20) #True
+print has_teen(10, 20, 29) #False
 
 # TODO - write not_string
 
